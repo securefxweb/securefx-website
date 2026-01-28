089b4940011f4053d52260a95de26cd6a4425352
Kevin <kevin088.rejoice@gmail.com>
2026-01-28 15:56:23 +0530

    styles:updated header ui

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import styles from "./header.module.scss";
 import classNames from "classnames";
@@ -9,46 +9,101 @@ const LoginIcon = "/assets/icons/login.svg";
 const Logo = "/assets/logo/logo.svg";
 export default function Header() {
 	const pathname = usePathname();
+	const [logindropdown, setLogindropdown] = useState(false);
 	return (
 		<div
 			className={classNames(styles.headerSticky, {
-				[styles.ibRoute]: pathname.startsWith("/ib"),
+				// [styles.ibRoute]: pathname.startsWith("/ib"),
 			})}
 		>
 			<header className={classNames(styles.header)}>
-				<Link href="#">
-					<div className={styles.buttonAlignment}>
-						<div className={styles.buttonUi}>
-							<div className={styles.layer}></div>
-							<div className={styles.layer2}></div>
-							<span>Try Demo</span>
-							<img src={ArrowIcon} alt="ArrowIcon" />
+				<div className={styles.buttonAlignment}>
+					<div className={styles.buttonUi}>
+						<div className={styles.layer}></div>
+						<div className={styles.layer2}></div>
+						<span>Try Demo</span>
+						<img src={ArrowIcon} alt="ArrowIcon" />
+					</div>
+				</div>
+				<div className={styles.loginButton}>
+					<div className={styles.loginbuttonUi} onClick={() => setLogindropdown(!logindropdown)}>
+						<div className={styles.layer}></div>
+						<div className={styles.layer2}></div>
+						<span>Log In</span>
+						<img className={classNames(logindropdown ? styles.rotate : "")} src={LoginIcon} alt="LoginIcon" />
+					</div>
+					<div className={classNames(styles.logindropdown, logindropdown ? styles.show : styles.hide)}>
+						<div className={styles.design}>
+							<a>User Login
+							</a>
+							<a>IB Login
+							</a>
+							<a>Affiliate Login
+							</a>
+							<a>Mt4/Mt5
+							</a>
 						</div>
 					</div>
-				</Link>
-				<Link href="#">
-					<div className={styles.loginButton}>
-						<div className={styles.loginbuttonUi}>
-							<div className={styles.layer}></div>
-							<div className={styles.layer2}></div>
-							<span>Log In</span>
-							<img src={LoginIcon} alt="LoginIcon" />
-						</div>
-					</div>
-				</Link>
+				</div>
 				<div className={styles.headerAlignment}>
-					<Link href="/about">About</Link>
-					<Link href={"/trading-platform"}>Trade</Link>
-					<Link href="/#accounts">Accounts</Link>
+					<Link className={styles.spacing} href="/about">
+						About
+						<div className={styles.dropdown}>
+							<div className={styles.dropdownDesign}>
+								<a>About us</a>
+								<a>Regulation</a>
+								<a>Legal Document
+								</a>
+								<a>Security of funds</a>
+							</div>
+						</div>
+					</Link>
+					<Link className={styles.spacing} href="/about">
+						Trade
+						<div className={styles.dropdown}>
+							<div className={styles.dropdownDesign}>
+								<a>Forex</a>
+								<a>Metal</a>
+								<a>Indices
+								</a>
+								<a>Energy</a>
+								<a>Commodities</a>
+								<a>Mt4/Mt5 Mobile</a>
+								<a>Economic Calendar</a>
+								<a>Calculators</a>
+							</div>
+						</div>
+					</Link>
+					<Link className={styles.spacing} href="/about">
+						Accounts
+						<div className={styles.dropdown}>
+							<div className={styles.dropdownDesign}>
+								<a>Standard</a>
+								<a>Pro</a>
+								<a>RAW
+								</a>
+								<a>ZERO Spread</a>
+								<a>Deposite/Withdraw</a>
+							</div>
+						</div>
+					</Link>
 					<Link href="/">
 						<img src={Logo} alt="Logo" />
 					</Link>
-					<Link href="/#partner">Partner</Link>
+					<Link className={styles.spacing} href="/about">
+						Partner
+						<div className={styles.dropdown}>
+							<div className={styles.dropdownDesign}>
+								<a>IB</a>
+								<a>Affiliate</a>
+							</div>
+						</div>
+					</Link>
 					<Link href="#">News</Link>
 					<Link href="mailto:support@securefx.net">Support</Link>
 					<Link href={"#"}>EN</Link>
 				</div>
-			</header>
-		</div>
+			</header >
+		</div >
 	);
 }
