034769773739a8a2c13edd01d21e3dfaedd64d52
Saksham <saksham369.rejoice@gmail.com>
2026-02-12 22:00:09 +0530

    Change demo label to live label in index.js

--- src/rendering/homePage/worldsTrustedForm/index.js
+++ src/rendering/homePage/worldsTrustedForm/index.js
@@ -385,7 +385,7 @@ export default function WorldsTrustedForm({
 								>
 									<div className={styles.layer}></div>
 									<div className={styles.layer2}></div>
-									<span>Try demo A/C</span>
+									<span>Try live A/C</span>
 								</div>
 							</div>
 						)}
